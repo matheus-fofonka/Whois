7508d286f3f5b5fe80ebddbb6284a87e599dacb9
matheus-fofonka <41707468+matheus-fofonka@users.noreply.github.com>
2018-12-20 15:07:42 -0200

    xml working, bug fixes in status and in serverList

--- Whois.apresentation/WhoisInterface/WhoisIndex.aspx.cs
+++ Whois.apresentation/WhoisInterface/WhoisIndex.aspx.cs
@@ -16,47 +16,35 @@ namespace Whois.apresentation.WhoisInterface
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        Dominio dom = new Dominio();
         public bool av;
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             ListBox1.Items.Clear();
-            LoadServerList();
-        }
 
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
+            AddInfo();
 
         }
 
-        public void LoadServerList()
-        {
-            var whois = new WhoisLookup();
-            var response = whois.Lookup(txtURL.Text);
 
-            var servers = response.ParsedResponse.NameServers;
-            foreach (string value in servers)
-            {
-                ListBox1.Items.Add(value);
-            }
-        }
 
-        public void ActionForSearch()
+        public void AddInfo()
         {
             if (txtURL.Text != "" && txtURL.Text != "digite uma URL" && txtURL.Text != "Domínio está registrado. " && txtURL.Text != "Domínio disponível para registro. ")
             {
-                Dominio dom = new Dominio();
                 var whois = new WhoisLookup();
                 var response = whois.Lookup(txtURL.Text);
                 string sub = (response.Content).Substring(0, 12);
 
-
                 if (sub != "No match for")
                 {
+
                     av = false;
                     txtURL.Text = "Domínio está registrado. ";
+
+                    lblSTATUS.ForeColor = System.Drawing.Color.Red;
+                    lblSTATUS.Text = "Registrado";
 
                     lblDomain.Text = response.ParsedResponse.DomainName.ToString();
                     dom.NmDomain = response.ParsedResponse.DomainName.ToString();
@@ -67,13 +55,31 @@ namespace Whois.apresentation.WhoisInterface
                     lblDtExpiration.Text = response.ParsedResponse.Expiration.ToString();
                     dom.DtExpiration = response.ParsedResponse.Expiration.ToString();
 
+                    var servers = response.ParsedResponse.NameServers;
+                    foreach (string value in servers)
+                    {
+                        ListBox1.Items.Add(value);
+                    }
+
                 }
-                else { av = true; txtURL.Text = "Domínio disponível para registro. "; }
+                else
+                {
+                    lblSTATUS.ForeColor = System.Drawing.Color.Green;
+                    lblSTATUS.Text = "Pronto pra registro";
+                    av = true;
+                    txtURL.Text = "Domínio disponível para registro. ";
+                    ListBox1.Items.Add("Ainda não possui servidores.");
+                }
 
                 dom.Exportar();
             }
             else { txtURL.Text = ("digite uma URL"); };
 
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
         }
     }
 }
